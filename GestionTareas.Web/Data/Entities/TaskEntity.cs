eb6773748d85758f502c52a41db5e49cd567503c
mateo <mateomunoz@gmail.com>
2025-10-31 23:07:02 -0500

    TaskEntity creada

--- GestionTareas.Web/Data/Entities/TaskEntity.cs
+++ GestionTareas.Web/Data/Entities/TaskEntity.cs
@@ -1,6 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GestionTareas.Web.Data.Entities;
 
 public class TaskEntity
 {
+    [Key]
+    public Guid Id { get; set; }
     
+    [Required]
+    [MaxLength(32)]
+    public required string Title { get; set; }
+    
+    [MaxLength(264)]
+    public string? Description { get; set; }
+    
+    public DateTime Created { get; set; }
+    public DateTime Finished { get; set; }
+    
+    public bool status { get; set; }
 }
